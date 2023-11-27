48fcfefcc5c3e6e6ccb6f3c8586fb101c9954c74
Temidire <temidireowoeye@gmail.com>
2023-11-27 03:39:12 +0100

    built minicard component

---
+++ src/components/Minicard.tsx
@@ -0,0 +1,18 @@
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Star from './Star';
+
+const Minicard = () => {
+  return (
+    <div className='grid grid-cols-[auto,1fr] bg-[#F7F0F6] max-w-max h=[137px] p-4 gap-x-4 relative'>
+        <div className='w-24 h-24 bg-red-400' />
+        <div className='text-base w-44'>
+            <p className='font-playfair'>Red ankara gown sumami crimison black</p>
+            <p className='font-montserrat'>₦ 30,000</p>
+            <Star filled/>
+        </div>
+        <AddCircleIcon className='absolute bottom-2 right-2' style={{fontSize: 30}}/>
+    </div>
+  )
+}
+
+export default Minicard
