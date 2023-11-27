8112d5eafba88235995a3670cd5ca7fcd6390437
Temidire <temidireowoeye@gmail.com>
2023-11-27 03:39:31 +0100

    changed navbar font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Navbar = () => {
     const navLinks = [
@@ -21,19 +22,22 @@ const Navbar = () => {
     }
   return (
     <>
-    <nav>
-        <div className='w-full flex justify-between items-center'>
+    <nav className=''>
+        <div className='w-full flex justify-between items-center px-[82px] py-[35px] fixed z-10 font-montserrat'>
             <ul className='flex space-x-24 text-xl font-normal'>
                 {navLinks.map((link) => (<li key={link.id}><a href="">{link.title}</a></li>))}
             </ul>
             <span className='text-4xl font-medium'>
                 Chapter40
             </span>
-            <ul className='flex justify-between items-center space-x-24 text-xl font-normal'>
-                <li><a href="#">Contact Us</a></li>
-                <li><PersonOutlineOutlinedIcon style={iconSize}/></li>
-                <li><ShoppingCartOutlinedIcon style={iconSize}/></li>
-            </ul>
+            <div className='flex justify-between items-center space-x-24 text-xl font-normal'>
+                <a href="#">Contact Us</a>
+                <ul className='space-x-5 flex'>
+                    <li><PersonOutlineOutlinedIcon style={iconSize}/></li>
+                    <li><FavoriteBorderOutlinedIcon /></li>
+                    <li><ShoppingCartOutlinedIcon style={iconSize}/></li>
+                </ul>
+            </div>
         </div>
     </nav>
     </>
