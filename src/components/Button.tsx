b60e7b6a8f6fff1705d0a86c03baa9c92d6192b0
Temidire <temidireowoeye@gmail.com>
2023-11-28 15:50:31 +0100

    updated button component and font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,21 @@
 interface ButtonProps {
-    text: String,
+  text: String;
+  paddingX?: String;
+  bgColor?: String;
+  textColor?: String;
 }
 
-const Button: React.FC<ButtonProps> = ({text}) => {
+const Button: React.FC<ButtonProps> = ({
+  textColor = "text-white",
+  paddingX = "px-8",
+  bgColor = "bg-[#36254B]",
+  text = "",
+}) => {
   return (
-        <button className="px-8 py-4 text-white bg-[#984288]">{text}</button>
-  )
-}
+    <button className={`${paddingX} py-4 ${textColor} ${bgColor} font-roboto`}>
+      {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
