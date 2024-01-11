8f3ca4a6923ddd2361f592c5ec46d1949f90feab
Obasan Tolulope <obasantolu@gmail.com>
2024-01-11 21:58:53 +0100

    Adjusted the layout and responsiveness

--- src/pages/NavigationPage.tsx
+++ src/pages/NavigationPage.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MyCart from "../components/MyCart";
 import NavigationMenu from "../components/navigationmenu/NavigationMenu";
-import { navigationItems } from "../components/constants/navigationItems";
 
 const NavigationPage = () => {
   return (
     <>
       <div className="lg:flex min-h-screen">
-        <div className="px-[50px] lg:pl-[108px] py-[40px] lg:py-[32px] pr-[65px] lg:w-1/2 font-playfair">
-          <NavigationMenu navigationItems={navigationItems} />
+        <div className="px-5 md:px-20 lg:px-10 py-[40px]   lg:w-1/2 font-playfair">
+          <NavigationMenu />
           <Outlet />
         </div>
         <div className="hidden lg:block lg:w-1/2 bg-[#F3E9FA] min-h-[screen]">
