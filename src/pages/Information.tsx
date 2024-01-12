ceb56f83d9a51ff419f8d0f91847f2d76a68f63b
Obasan Tolulope <obasantolu@gmail.com>
2024-01-12 16:34:54 +0100

    Added Error messages

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,6 +1,4 @@
 import { Link, useNavigate } from "react-router-dom";
-import Person4OutlinedIcon from "@mui/icons-material/Person4Outlined";
-import PhoneIphoneOutlinedIcon from "@mui/icons-material/PhoneIphoneOutlined";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -13,16 +11,19 @@ interface Props {
 }
 
 const schema = z.object({
-  state: z.string(),
-  email: z.string().min(5, "not up to 5 characters"),
-  firstName: z.string(),
-  lastName: z.string(),
+  state: z.string().min(3, { message: "State should be a least 3 characters" }),
+  email: z
+    .string()
+    .min(5, "not up to 5 characters")
+    .email("The email format you entered is invalid"),
+  firstName: z.string().min(3, "Name should have at least 3 characters"),
+  lastName: z.string().min(3, "Name should have at least 3 characters"),
   company: z.string(),
-  address: z.string(),
+  address: z.string().min(5, "Address should contain at least 5 characters"),
   apartment: z.string(),
-  postalCode: z.string().min(6).max(6),
-  city: z.string(),
-  phone: z.string().min(5),
+  postalCode: z.string().min(6, "Postal code should be at least 6 digits"),
+  city: z.string().min(3),
+  phone: z.string().min(5, "Number should be at least 5 digits"),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -37,10 +38,9 @@ const Information = ({ onSubmit }: Props) => {
     formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onBlur" });
 
-
   const inputContainerStyles = {
-    base: "lg:w-1/2 border border-[#606060] px-[16px] py-2",
-    input: "outline-none w-full",
+    base: "lg:w-1/2 border border-[#606060]",
+    input: "outline-none px-[16px] py-2 w-full",
   };
   const allowOnlyNumbers: KeyboardEventHandler<HTMLInputElement> = (evt) => {
     const keysAllowed: string[] = [
@@ -90,24 +90,23 @@ const Information = ({ onSubmit }: Props) => {
             )}
           </div>
           <div
-            className={`flex border items-center border-[#606060] px-[16px] ${
+            className={`flex border items-center border-[#606060]  ${
               errors.email ? "border-red-600" : ""
-            } py-2 space-x-[4px]`}
+            } `}
           >
-            <Person4OutlinedIcon fontSize="small" className="text-[#606060]" />
             <div className="flex-1">
               <input
                 {...register("email")}
                 type="email"
                 placeholder="Email"
                 required
-                className={`font-montserrat  outline-none w-full text-[#606060]`}
+                className="font-montserrat py-2 px-[16px]  outline-none w-full text-[#606060]"
               />
             </div>
           </div>
-              {errors.email && (
-                <p className="text-red-600">{errors.email.message}</p>
-              )}
+          {errors.email && (
+            <p className="text-red-600">{errors.email.message}</p>
+          )}
           <div className="flex space-x-1 mt-1 mb-[28px]">
             <input type="checkbox" className="rounded-none" />
             <p className="font-montserrat text-[#0C0C0C] text-[14px]">
@@ -118,17 +117,24 @@ const Information = ({ onSubmit }: Props) => {
             Shipping Address
           </header>
           <div className="space-y-[8px] font-montserrat">
-            <div className="flex justify-between border border-[#606060] px-[16px] py-2 space-x-[4px]">
+            <div className="flex justify-between border border-[#606060]  space-x-[4px]">
               <input
                 {...register("state")}
                 type="text"
                 placeholder="State"
                 required
-                className="outline-none w-full text-[#606060]"
+                className="outline-none px-[16px] py-2 w-full text-[#606060]"
               />
             </div>
+            {errors.state && (
+              <p className="text-red-600">{errors.state.message}</p>
+            )}
             <div className="space-y-[8px] lg:space-y-0 lg:flex lg:space-x-[24px] font-montserrat">
-              <div className={inputContainerStyles.base}>
+              <div
+                className={`${inputContainerStyles.base} ${
+                  errors.firstName ? "border-red-600" : ""
+                }`}
+              >
                 <input
                   {...register("firstName")}
                   type="text"
@@ -137,7 +143,12 @@ const Information = ({ onSubmit }: Props) => {
                   className={inputContainerStyles.input}
                 />
               </div>
-              <div className={inputContainerStyles.base}>
+
+              <div
+                className={`${inputContainerStyles.base} ${
+                  errors.lastName ? "border-red-600" : ""
+                }`}
+              >
                 <input
                   {...register("lastName")}
                   type="text"
@@ -147,33 +158,37 @@ const Information = ({ onSubmit }: Props) => {
                 />
               </div>
             </div>
-            <div className="flex justify-between border border-[#606060] px-[16px] py-2">
+            <div className="flex justify-between border border-[#606060]">
               <input
                 {...register("company")}
                 type="text"
                 placeholder="Company(Optional)"
-                className="outline-none w-full"
+                className="outline-none px-[16px] py-2 w-full"
               />
             </div>
-            <div className="flex justify-between border border-[#606060] px-[16px] py-2">
+            <div className="flex justify-between border border-[#606060] ">
               <input
                 {...register("address")}
                 type="text"
                 placeholder="Address"
                 required
-                className="outline-none w-full"
+                className="outline-none px-[16px] py-2 w-full"
               />
             </div>
-            <div className="flex justify-between border border-[#606060] px-[16px] py-2">
+            <div className="flex justify-between border border-[#606060]">
               <input
                 {...register("apartment")}
                 type="text"
                 placeholder="apartment,suite,etc.(optional)"
-                className="outline-none w-full"
+                className="outline-none px-[16px] py-2 w-full"
               />
             </div>
             <div className="space-y-[8px] lg:space-y-0 lg:flex lg:space-x-[24px] font-montserrat">
-              <div className={inputContainerStyles.base}>
+              <div
+                className={`${inputContainerStyles.base} ${
+                  errors.postalCode ? "border-red-600" : ""
+                }`}
+              >
                 <input
                   {...register("postalCode")}
                   type="text"
@@ -183,7 +198,11 @@ const Information = ({ onSubmit }: Props) => {
                   onKeyDown={allowOnlyNumbers}
                 />
               </div>
-              <div className={inputContainerStyles.base}>
+              <div
+                className={`${inputContainerStyles.base} ${
+                  errors.city ? "border-red-600" : ""
+                }`}
+              >
                 <input
                   {...register("city")}
                   type="text"
@@ -193,24 +212,24 @@ const Information = ({ onSubmit }: Props) => {
                 />
               </div>
             </div>
-            <div className="flex justify-between border border-[#606060] px-[16px] py-2 space-x-[4px]">
-              <div className="flex-1">
-                <input
-                  {...register("phone")}
-                  type="text"
-                  placeholder="phone"
-                  required
-                  className="outline-none w-full text-[#606060]"
-                  onKeyDown={allowOnlyNumbers}
-                />
-              </div>
-              <PhoneIphoneOutlinedIcon
-                fontSize="small"
-                className=" text-[#606060]"
+            <div
+              className={`flex justify-between border ${
+                errors.phone ? "border-red-600" : ""
+              } border-[#606060] space-x-[4px]`}
+            >
+              <input
+                {...register("phone")}
+                type="text"
+                placeholder="phone"
+                required
+                className="outline-none w-full px-[16px] py-2 text-[#606060]"
+                onKeyDown={allowOnlyNumbers}
               />
             </div>
+            {errors.phone && (
+              <p className="text-red-600">{errors.phone.message}</p>
+            )}
           </div>
-
 
           <div className="flex space-x-1 mt-1 mb-[80px]">
             <input type="checkbox" className="rounded-none" />
@@ -223,17 +242,16 @@ const Information = ({ onSubmit }: Props) => {
             <button
               disabled={!isValid}
               type="submit"
-              onClick={() => setTimeout(() => {
-                navigate("/navigation-page/shipping"), 3000
-        
-              })}
+              onClick={() =>
+                setTimeout(() => {
+                  navigate("/navigation-page/shipping"), 3000;
+                })
+              }
               className="bg-darkPrimary w-full lg:w-auto px-[32px] hover:cursor-pointer py-[16px] text-white font-roboto"
             >
               <p>Continue to Shipping</p>
             </button>
             <div className="flex space-x-2 font-montserrat text-[#634D93] text-[16px]">
-
-
               <button onClick={() => navigate("/cart")}>
                 <ArrowBackIosNewOutlinedIcon />
               </button>
