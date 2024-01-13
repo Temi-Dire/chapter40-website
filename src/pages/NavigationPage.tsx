acdaf61f3a2c8d73a1326063bbf44793f4d8ca20
Obasan Tolulope <obasantolu@gmail.com>
2024-01-13 09:05:41 +0100

    Removed unused components

--- src/pages/NavigationPage.tsx
+++ src/pages/NavigationPage.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import MyCart from "../components/MyCart";
-import NavigationMenu from "../components/navigationmenu/NavigationMenu";
 
 const NavigationPage = () => {
   return (
     <>
       <div className="lg:flex min-h-screen">
         <div className="px-5 md:px-20 lg:px-10 py-[40px]   lg:w-1/2 font-playfair">
-          <NavigationMenu />
+          <header className="text-[24px] lg:text-4xl mb-[12px] lg:mb-[29px] text-black">
+            Chapter40
+          </header>
           <Outlet />
         </div>
         <div className="hidden lg:block lg:w-1/2 bg-[#F3E9FA] min-h-[screen]">
