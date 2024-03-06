f9506027f8c8f60a6c6cb13e50175a706e8d64f2
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-03-06 14:37:05 +0100

    worked on the payment page

--- src/components/OnlinePayment.tsx
+++ src/components/OnlinePayment.tsx
@@ -1,12 +1,50 @@
+import { PaystackButton } from "react-paystack";
+import useInformationStore from "../store/shippingInfo";
+import useStore from "../State";
 
-const OnlinePayment = () => {
+const PaystackPayment = () => {
+  
+
+  const publicKey = import.meta.env.VITE_PUBLIC_KEY; 
+
+  const onSuccess = (reference: string) => {
+    console.log("Payment successful", reference);
+    // and here too as well
+  };
+
+  const onClose = () => {
+    console.log("Payment closed");
+
+    // remember to use toastify here
+  };
+  const {email} = useInformationStore();
+  const store = useStore();
+  const basket = store.basket;
+
+  const getTotal = () => {
+    let sum: number = 0;
+    for (let i = 0; i < basket.length; i++) {
+      sum += basket[i].price * basket[i].quantity;
+    }
+    return sum;
+  };
+
+
+  const config = {
+    email,
+    amount: getTotal() * 100,
+    publicKey,
+    text: "Click here to pay now",
+    onSuccess,
+    onClose,
+  };
+
   return (
-    <div>
-
-        
-      
+    <div className="max-w-md my-8 p-4 bg-white shadow-md  font-montserrat rounded-md">
+      <h1 className="text-2xl font-semibold mb-4">Pay with Paystack</h1>
+      <PaystackButton {...config} />
     </div>
-  )
-}
+  );
+};
 
-export default OnlinePayment
+export default PaystackPayment;
