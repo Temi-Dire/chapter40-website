d9d9697f1c31773beff44db1954a3b4eb1d014a1
Temidire <temidireowoeye@gmail.com>
2024-03-06 10:29:59 +0100

    fix

--- src/components/SavedProduct.tsx
+++ src/components/SavedProduct.tsx
@@ -1,10 +1,9 @@
 interface SavedProductProps {
-  desc: string,
-  price: number,
-  id: number,
+  desc: string;
+  price: number;
 }
 
-const SavedProduct: React.FC<SavedProductProps> = ({desc, price, id}) => {
+const SavedProduct: React.FC<SavedProductProps> = ({ desc, price }) => {
   return (
     <>
       <div className="h-28 flex items-center mb-4">
@@ -15,9 +14,7 @@ const SavedProduct: React.FC<SavedProductProps> = ({desc, price, id}) => {
         />
         <div className="flex justify-between w-full items-center h-full ">
           <div className="flex flex-col justify-between w-44 h-full ">
-            <div className="font-playfair">
-              {desc}
-            </div>
+            <div className="font-playfair">{desc}</div>
             <button className="bg-[#36254B] w-[125px] px-2 py-2 text-white">
               Add to Cart
             </button>
