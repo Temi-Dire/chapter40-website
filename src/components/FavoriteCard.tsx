0e7d12336c1f6e1d082e7a649d124ea454909b23
Obasan Tolulope <obasantolu@gmail.com>
2024-03-08 11:40:01 +0100

    Created a wishlist page

---
+++ src/components/FavoriteCard.tsx
@@ -0,0 +1,40 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+import useStore from "../State";
+
+interface Props {
+  id: number;
+  image?: string;
+  desc: string;
+  price: number;
+}
+
+const FavoriteCard = ({ id, image, desc, price }: Props) => {
+  const { addToBasket, removeFromFavorites } = useStore();
+  return (
+    <div className="border shadow-md border-borderDark p-4 mb-5 flex items-center">
+      <div className="mr-8">
+        <img src={image} alt={`dress ${id}`} />
+      </div>
+      <div className="flex flex-col space-y-8">
+        <p className="leading-tight">{desc}</p>
+        <p className="font-montserrat">{price}</p>
+      </div>
+      <div className="ml-auto flex flex-col space-y-8">
+        <button
+          onClick={() => addToBasket({ desc, price, id })}
+          className=" uppercase bg-lightPrimary bg-opacity-80 hover:bg-opacity-100 text-white font-montserrat border-none text-[13px] border py-1 px-4 cursor-pointer rounded-sm "
+        >
+          Add to Cart
+        </button>
+        <button
+          onClick={() => removeFromFavorites(id)}
+          className=" flex uppercase justify-center"
+        >
+          <FaRegTrashAlt />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteCard;
--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,15 +1,49 @@
+import useStore from "../State";
+import FavoriteCard from "../components/FavoriteCard";
 import Navbar from "../components/Navbar";
 import FooterSection from "../sections/footer";
 
 const Favorite = () => {
+  const { favorites } = useStore();
+
+  // const totalFavorites = () => {
+  //   let sum = 0;
+  //   for (let i = 0; i < favorites.length; i++) {
+  //     sum += favorites[i].quantity;
+  //   }
+  //   return sum;
+  // };
+
   return (
-    <div>
-          <Navbar />
-          <div className="">
-              
+    <>
+      <Navbar />
+
+      <div className="flex my-12 justify-center flex-col px-4 sm:px-10 xl:px-[250px]">
+        {favorites.length !== 0 ? (
+          <>
+            <div className="flex flex-col">
+              {" "}
+              {/* Centering wrapper */}
+              <p className="">Saved Items({favorites.length})</p>
+              {favorites.map((favorite) => (
+                <FavoriteCard
+                  key={favorite.id}
+                  desc={favorite.desc}
+                  price={favorite.price}
+                  id={favorite.id}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <div>
+            <p>No items have been saved</p>
           </div>
-          <FooterSection/>
-    </div>
+        )}
+      </div>
+
+      <FooterSection />
+    </>
   );
 };
 
