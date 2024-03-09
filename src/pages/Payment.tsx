45373a9531c4477da6d71f17464da94859011944
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-03-09 22:52:53 +0100

    finished payment UI

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,5 +1,8 @@
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import OnlinePayment from "../components/OnlinePayment"; // Import your online payment form component
+
 
 const Payment = () => {
   const [selectedPaymentOption, setSelectedPaymentOption] = useState<
@@ -12,7 +15,16 @@ const Payment = () => {
 
   return (
     <>
-      <h2 className="mb-4 font-montserrat">Payment Method</h2>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className=" font-montserrat">Payment Method</h2>
+
+        <div className="flex space-x- items-center  font-montserrat text-[#36254B] text-[16px]">
+          <Link to="/cart/info" className="hover:text-darkPrimary">
+            <ArrowBackIosNewOutlinedIcon />
+            return to shipping
+          </Link>
+        </div>
+      </div>
       <div className="max-w-md p-4 border rounded-md font-montserrat">
         <div className="payment-option mb-4">
           <input
