03cbdc18ce7c47537535e25115eb6f49056ca71c
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-03-11 14:03:28 +0100

    fix

--- src/components/contactUsFooter.tsx
+++ src/components/contactUsFooter.tsx
@@ -1,8 +1,8 @@
 const FormFooter = () => {
   return (
     <div className="mt-4 font-poppins">
-      <div className="flex justify-between lg:text-sm max-w-full text-xs  flex-wrap   [&>*:not(:last-child)]:mb-5">
-        <div className="flex sm:mr-7">
+      <div className="flex justify-between items-center lg:text-sm max-w-full text-xs  flex-wrap   [&>*:not(:last-child)]:mb-5">
+        <div className="flex sm:mr-7 items-center">
           <div className="flex justify-center flex-shrink-0 items-center mr-2 ">
             <img
               src="/assets/images/phone.svg"
@@ -12,24 +12,14 @@ const FormFooter = () => {
           </div>
           <div>
             <p>PHONE</p>
-            <p className="text-[#36254B] whitespace-nowrap">Official number</p>
+            <p className="text-[#36254B] whitespace-nowrap">
+              08033231939
+            </p>
           </div>
         </div>
-        {/* <div className="flex mr-7">
-					<div className="flex justify-center items-center mr-2 flex-shrink-0 ">
-						<img
-							src="/assets/fax.svg"
-							className=" w-7 h-7 object-cover"
-							alt=""
-						/>
-					</div>
-					<div>
-						<p>FAX</p>
-						<p className="text-[#DD5471] whitespace-nowrap">03 5432 1234</p>
-					</div>
-				</div> */}
-        <div className="flex ">
-          <div className="flex justify-center items-center mr-2 flex-shrink-0">
+  
+        <div className="flex">
+          <div className="flex justify-center mr-2 flex-shrink-0">
             <img
               src="/assets/images/mail.svg"
               className="w-7 h-7	 object-cover"
@@ -38,7 +28,7 @@ const FormFooter = () => {
           </div>
           <div className="flex flex-col flex-wrap">
             <p>EMAIL</p>
-            <p className="text-[#36254B]]">official/site's email</p>
+            <p className="text-[#36254B]]">chapter40fashion@gmail.com</p>
           </div>
         </div>
       </div>
--- src/pages/ContactUsPage.tsx
+++ src/pages/ContactUsPage.tsx
@@ -1,17 +1,56 @@
 import ContactForm from "../components/ContactUsForm";
 import Navbar from "../components/Navbar";
 import FooterSection from "../sections/footer";
+// import FooterSection from "../sections/footer";
 
-const ContactUsPage = () => {
+// const ContactUsPage = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="w-full self-stretch">
+//         <div className="flex justify-center items-center flex-col mb-9 px-5">
+//           <h1 className="text-4xl font-bold mb-5 mt-16">Trying to reach us?</h1>
+//           <p className="text-gray-600 mb-6">
+//             We're more than open to hear from you. Whether you have questions,
+//             feedback, or thoughts, we'd love to connect with you.
+//           </p>
+//         </div>
+//         <div className="flex flex-col-reverse lg:flex-row gap-12 justify-between lg:mb-20 items-center relative sm:px-16 lg:px-32 sm:py-[50px] py-8 lg:py-[100px]">
+//           <div className="flex-1 h-full max-w-[545px] px-4 sm:px-0">
+//             <ContactForm />
+//           </div>
+//           <div className="lg:flex-1 z-20 max-w-[545px] lg:self-stretch h-[300px] lg:h-auto w-full mx-2.5">
+//             <img
+//               src="/assets/images/contactUs.jpeg"
+//               className=" object-cover "
+//               alt=""
+//             />
+//           </div>
+//           <div className="w-[20vw] self-end bg-[#36254B] hidden lg:block h-full max-h-[904px] z-10 absolute max-w-[426px] right-0 top-0" />
+//         </div>
+//       </div>
+//       <FooterSection />
+//     </>
+//   );
+// };
+
+// export default ContactUsPage;
+
+
+
+
+const ContactUs = () => {
   return (
     <>
       <Navbar />
-      <div className="w-full self-stretch">
+      <div className="w-full max-w-[1440px] self-stretch">
         <div className="flex justify-center items-center flex-col mb-9 px-5">
-          <h1 className="text-4xl font-bold mb-5 mt-16">Trying to reach us?</h1>
-          <p className="text-gray-600 mb-6">
+          <h1 className="mb-5 text-2xl md:text-4xl text-center font-bold font-poppins mt-14 ">
+            Trying to reach us?
+          </h1>
+          <p className="text-center font-poppins">
             We're more than open to hear from you. Whether you have questions,
-            feedback, or thoughts, we'd love to connect with you.
+             feedback, or thoughts, we'd love to connect with you.
           </p>
         </div>
         <div className="flex flex-col-reverse lg:flex-row gap-12 justify-between lg:mb-20 items-center relative sm:px-16 lg:px-32 sm:py-[50px] py-8 lg:py-[100px]">
@@ -21,16 +60,19 @@ const ContactUsPage = () => {
           <div className="lg:flex-1 z-20 max-w-[545px] lg:self-stretch h-[300px] lg:h-auto w-full mx-2.5">
             <img
               src="/assets/images/contactUs.jpeg"
-              className="h-3/4 object-cover"
+              className=" object-cover "
               alt=""
             />
           </div>
-          <div className="w-[20vw] self-end bg-[#36254B] hidden lg:block h-full max-h-[904px] z-10 absolute max-w-[426px] right-0 top-0" />
+          <div className="w-[20vw] self-end bg-[#183A4A] hidden lg:block h-full max-h-[904px] z-10 absolute max-w-[426px] right-0 top-0" />
         </div>
+        <FooterSection />
       </div>
-      <FooterSection />
+      
     </>
   );
+
 };
 
-export default ContactUsPage;
+export default ContactUs;
+
