b7c3057d4c0c0613fe33ed0e8d73ead8fb565927
Temidire <temidireowoeye@gmail.com>
2024-03-11 20:32:30 +0100

    added container

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,10 +1,11 @@
+import Container from "../components/Container";
 import Navbar from "../components/Navbar";
 import ContactUs from "../components/contactUs";
 const GetAquote = () => {
   return (
     <>
-    <Navbar/>
-      <div className="max-w-[1300px]  text-gray-300 mx-auto w-full lg:p-20 sm:mt-[50px] lg:mt-[100px] mb-[40px] mt-10 px-0 md:px-0 ">
+      <Navbar />
+      <Container className="text-gray-300 sm:mt-[50px] lg:mt-[100px] mb-[40px] mt-10">
         <div className=" w-full lg:flex gap-x-6 flex flex-col lg:flex-row justify-center ">
           <div className="bg-[#36254B] lg:rounded-tl-3xl py-10 lg:justify-center lg:items-center lg:rounded-bl-3xl lg:w-1/2 lg:flex flex-col p-5 sm:p-10  text-center">
             <h2 className="text-2xl mb-3">CONTACT US</h2>
@@ -14,13 +15,12 @@ const GetAquote = () => {
               feedback, or thoughts, we'd love to connect with you.
             </p>
             <img src="/assets/images/contactUs.jpeg" alt="" />
- 
           </div>
           <div className=" pt-10 lg:w-1/2">
             <ContactUs />
           </div>
         </div>
-      </div>
+      </Container>
     </>
   );
 };
