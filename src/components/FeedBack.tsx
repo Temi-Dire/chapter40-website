0e873c7ac6b5099f5f4d71b53cf8e8d9da06e067
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-03-20 11:55:56 +0100

    fix- contactUsPage

--- src/components/FeedBack.tsx
+++ src/components/FeedBack.tsx
@@ -1,10 +1,24 @@
+import { motion } from "framer-motion";
+
 const FeedBack = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full font-montserrat">
-      <h3 className="text-center text-4xl mb-2">Thank you!!</h3>
-      <p className="text-center text-2xl text-gray-600">
+      <motion.h3
+        className="text-center text-4xl mb-2"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 0.2 }}
+      >
+        Thank you!!
+      </motion.h3>
+      <motion.p
+        className="text-center text-2xl text-gray-600"
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 0.4 }}
+      >
         Your response has been recorded. We'll get back to you shortly
-      </p>
+      </motion.p>
     </div>
   );
 };
