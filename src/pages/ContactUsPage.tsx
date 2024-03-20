be3a742415324db79644389bdb7a556db3f39d68
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-03-20 10:37:10 +0100

    fix height issue

--- src/pages/ContactUsPage.tsx
+++ src/pages/ContactUsPage.tsx
@@ -14,10 +14,9 @@ const ContactUs = () => {
     <>
       <Navbar />
       <Container className="self-stretch xl:mt-9">
-        <div className="flex  flex-col-reverse xl:flex-row gap-12 justify-between lg:mb-20 items-center relative sm:px-16 xl:pl-0 sm:py-[50px] py-8 xl:py-[100px]">
+        <div className="flex  flex-col-reverse xl:flex-row gap-12 xl:h-[805px] justify-between lg:mb-20 items-center relative sm:px-16 xl:pl-0 sm:py-[50px] py-8 xl:py-[100px]">
           <div className="flex-1 h-full max-w-[545px] ">
-            <ContactForm />
-    
+            <ContactForm />    
           </div>
           <div className="lg:flex-1 z-20 max-w-[545px] lg:self-stretch h-[300px] mx-auto items-center  lg:h-auto w-full ">
             <img
